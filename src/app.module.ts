e9219082b4c0fd1b629a6742d2f3fec9351bba51
Xavier Carriba <xavier.carriba@gmail.com>
2025-05-20 02:28:24 +0200

    Acceptance testing dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TaskListsController } from './application/rest/task-lists/task-lists.controller';
+import { HealthModule } from './health.module';
+import { HealthController } from './application/rest/status/health.controller';
 
 @Module({
-  imports: [],
-  controllers: [TaskListsController],
+  imports: [HealthModule],
+  controllers: [TaskListsController, HealthController],
   providers: [],
 })
 export class AppModule {}
